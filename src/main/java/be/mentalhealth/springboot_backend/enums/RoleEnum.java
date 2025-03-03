77bcf4501d13b38f73afe4f222aa2bfee572d1e9
Leminh04 <151699827+Leminh04@users.noreply.github.com>
2025-03-03 08:12:24 +0700

    Update RoleEnum.java

--- src/main/java/be/mentalhealth/springboot_backend/enums/RoleEnum.java
+++ src/main/java/be/mentalhealth/springboot_backend/enums/RoleEnum.java
@@ -1,27 +1,5 @@
 package be.mentalhealth.springboot_backend.enums;
 
 public enum RoleEnum {
-<<<<<<< HEAD
-    MANAGER,
-    CUSTOMER
-=======
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    //enum là 1 hằng số
-    MANAGER,
-    CUSTOMER
-
-=======
     STUDENT, PARENT, PSYCHOLOGIST,CUSTOMER
->>>>>>> Stashed changes
-=======
-    MANAGER,
-    CUSTOMER
->>>>>>> Stashed changes
-=======
-    MANAGER,
-    CUSTOMER
->>>>>>> Stashed changes
->>>>>>> 5982d663ddbb7d04b583f75c1c58e950d57db4e7
 }
